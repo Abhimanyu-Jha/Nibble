e3b747a8177bdb0ec12b0f8c66ec0fb0bf4b3d00
unknown <abhimanyu.in@gmail.com>
2019-01-13 12:48:42 +0530

    religious commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //NODE MODULES
+var fs = require("fs");
 const express=require('express');
 const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
@@ -41,6 +42,7 @@ app.post('/search',urlencodedParser,(req,res)=>{
 	var minval=Number.MAX_VALUE;
 	var finalname;
 	var branch;
+	var jsonData = {temp:"ok"};
 	var arr=searchQuery.split(" ");
 	var q="";
 	for (i=0; i<arr.length; i++){
@@ -54,43 +56,64 @@ app.post('/search',urlencodedParser,(req,res)=>{
 	//'https://developers.zomato.com/api/v2.1/search?q='+q+'&lat='+lat+'&lon='+long
 	fetch('https://developers.zomato.com/api/v2.1/search?q='+q+'&lat='+latitude+'&lon='+longitude, {
 	    method: 'get',
-	    headers: { 'Content-Type': 'application/json', 'user-key': '4d85d015e7c7d33f1f4d85edd738bfbb' },
+	    headers: { 'Content-Type': 'application/json', 'user-key': '5cdc9aed20a04f2f6310c10ca2b7dfd8' },
 
 	})
 	.then(res => res.json())
 	.then(function(json){
-	    var myarr=json.restaurants;
-	    for (i=0;i<myarr.length;i++){
-	    	var x=myarr[i];
-	    	// console.log(x);
-	    	var a=0;
-	    	// console.log(x.restaurant.name.toLowerCase(),"blah");
-	    	if (x.restaurant.name.toLowerCase()==searchQuery){
+		// EXTRACT NAME OF CLOSEST AND MOST RELVANT RESTAURANT
+		function is_inside(s1,s2)
+		{	s1=s1.toLowerCase();
+			s2=s2.toLowerCase();
 
-	    		a=Math.abs(latitude-x.restaurant.location.latitude)+Math.abs(longitude-x.restaurant.location.longitude);
-	    		if (a<minval){
-	    			minval=a;
-	    			finalname=x.restaurant.name;
-	    			branch=x.restaurant.location.locality;
-	    		}
-	    	}
-	    }
-	    // console.log(finalname+' '+branch);
+			s2=s2.replace(/[^a-zA-Z0-9 ]/g, "");
+			s1=s1.replace(/[^a-zA-Z0-9 ]/g, "");
+			return s2.includes(s1);
+		}
+		console.log(json);
+		   var myarr=json.restaurants;
+		   for (i=0;i<myarr.length;i++){
+		   	var x=myarr[i];
+		   	// console.log(x);
+		   	var a=0;
+		   	// console.log(x.restaurant.name.toLowerCase(),"blah");
+		   	if (is_inside(searchQuery,x.restaurant.name)!=-1){
+		   		a=Math.abs(latitude-x.restaurant.location.latitude)+Math.abs(longitude-x.restaurant.location.longitude);
+		   		if (a<minval){
+		   			minval=a;
+		   			finalname=x.restaurant.name;
+		   			branch=x.restaurant.location.locality;
+		   		}
+		   	}
+		   }
+		   console.log('OUTLET',finalname+' '+branch);
+		return [finalname,branch];
+
+		// return ['Burger Singh','Kalkaji'];
+	}).then(function(finalname_branch){
+		//SIDHANT AND AYAAN USE FILENAME FOR RESTERAUNT NAME AND BRANCH FOR LOCATION
+		//
+		var spawn = require("child_process").spawn;
+		console.log('NAMES',finalname_branch[0],finalname_branch[1]);
+
+		var process = spawn('python',["./main.py",finalname_branch[0],finalname_branch[1]]);
+
+		process.stdout.on('data', function(data) {
+	        jsonData = JSON.parse(data.toString());
+	        console.log('jsonDATA',jsonData);
+		});
+
+		// return data={
+		// 	restaurant_name:'Burger Singh',
+		// 	location:'Kalkaji Mandir',
+		// 	menu:{'Potato Crunch Burger':['description',['₹68','₹100']]},
+		// 	links:['swiggy.com','zomato.com'],
+		// 	photo_link:"http://www.dwarka21.com/uploads/images/burger-1.jpg"
+		// };
+		return jsonData;
+	}).then((jsonData)=>{
+		res.render('results',{data: jsonData})
 	});
-	//SIDHANT AND AYAAN USE FILENAME FOR RESTERAUNT NAME AND BRANCH FOR LOCATION
-
-
-	//FINALLY PREPARE ME THIS JSON OBJECT SIMILAR TO THIS SO THAT I CAN AUTOMATE FRONTEND
-	data={
-		restaurant_name:'Burger Singh',
-		location:'Kalkaji Mandir',
-		menu:{'Potato Crunch Burger':['description',['₹68','₹100']]},
-		links:['swiggy.com','zomato.com'],
-		photo_link:"http://www.dwarka21.com/uploads/images/burger-1.jpg"
-	};
-
-	//THIS SHOULD BE AT THE END
-	res.render('results',{data: data});
 
 });
 
---
+++ main.py
@@ -0,0 +1,104 @@
+import sys
+import json
+
+import requests
+from contextlib import closing
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
+
+def get_zomato_menu(restaurant, location):
+	restaurant = "+".join(restaurant.split()) + "+"
+	location = "+".join(location.split()) + "+"
+	urlLink = "https://www.google.com/search?q=" + restaurant + location + "zomato order"
+	google = requests.get(urlLink).text
+	gSoup = BeautifulSoup(google, 'lxml')
+	data = gSoup.find("h3", class_="r").a["href"]
+	start = data.find("https")
+	end = data.find("&",start)
+
+	url = data[start:end] + "/order"
+
+	menu={}
+	# use chrome to get page with javascript generated content
+	with closing(Chrome()) as browser:
+		browser.get(url)
+		# wait for the page to load
+		WebDriverWait(browser, timeout=100).until(
+			lambda x: x.find_elements_by_xpath("//*[contains(text(), 'Bestsellers')]"))
+		# store it to string variable
+		page_source = browser.page_source
+	soup=BeautifulSoup(str(page_source.encode('utf-8')),features="html.parser")
+	for menu_tag in soup.find_all('div',class_='header'):
+		item=menu_tag.text;
+		parent_tag=menu_tag.parent
+
+		parent_tag_str=str(parent_tag)
+
+		dis_price=parent_tag_str.find('react-text')
+		menu[item]=['']
+		if dis_price!=-1:
+			dis_price_text_si=parent_tag_str.find('-->',dis_price)
+
+			text=parent_tag_str[dis_price_text_si+3:parent_tag_str.find('<!--',dis_price_text_si)]
+
+			menu[item].append(float(text[text.rfind('\\')+4:]))
+		else:
+			text=parent_tag.find('span').text
+			menu[item].append(float(text[text.rfind('\\')+4:]))
+
+	return menu,url
+
+def searchSwiggy(restaurant, location):
+    restaurant = "+".join(restaurant.split()) + "+"
+    location = "+".join(location.split()) + "+"
+    url = "https://www.google.com/search?q=" + restaurant + location + "swiggy"
+    google = requests.get(url).text
+    gSoup = BeautifulSoup(google, 'lxml')
+    data = gSoup.find("h3", class_="r").a["href"]
+    start = data.find("https")
+    end = data.find("&",start)
+
+    restaurantLink = data[start:end]
+
+    restaurant = requests.get(restaurantLink).text
+
+    soup = BeautifulSoup(restaurant,'lxml')
+    items = soup.find_all("div", class_="_19GqV")
+
+    menu={}
+    for x in items:
+        item = x.find("div",class_="jTy8b").text
+
+        try:
+            description = x.find("div",class_="_2aOqz _1xb2E").text
+
+        except:
+            description = ''
+
+        temp = str(x.find("div",class_="_12lpv MwITc"))
+        try:
+            price = temp[temp.index(">",temp.index('"bQEAj"'))+1:temp.index("<",temp.index('"bQEAj"'))]
+        except:
+            price = temp[temp.index(">",temp.index("_22I-3 bQEAj grcH5"))+1:temp.index("<",temp.index("_22I-3 bQEAj grcH5"))]
+
+        menu[item] = [description,price]
+
+    return menu,restaurantLink
+
+
+def funcMerger(restaurant,location):
+	swiggyData,swiggyLink = searchSwiggy(restaurant,location)
+	zomatoData,zomatoLink = get_zomato_menu(restaurant,location)
+	menu={}
+	for item in swiggyData:
+		try:
+			menu[item] = [swiggyData[item][0],[float(swiggyData[item][-1]),zomatoData[item][-1]]]
+		except:
+			menu[item] = [swiggyData[item][0],[float(swiggyData[item][-1]),"NA"]]
+
+
+	return json.dumps({'menu':menu,'links':[swiggyLink,zomatoLink]})
+
+print(funcMerger(sys.argv[1],sys.argv[2]))
